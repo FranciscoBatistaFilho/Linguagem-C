25251fc8f9499356aba48f094d2ddf05b4e939b0
Francisco Batista <77498087+FranciscoBatistaFilho@users.noreply.github.com>
2021-01-19 08:04:32 -0300

    add notes

--- Exercicios em C/001.c
+++ Exercicios em C/001.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("welcome, to c!\n");
+    printf ("welcome, to c!\n");
 }
 
 //end of program
+
+/*sequence of scape
+
+\n -- skip a line
+\\ -- (blackslash) print \
+\* -- print *
+
+*/
